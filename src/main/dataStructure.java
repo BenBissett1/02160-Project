6b57e620342e0c7ea9cdcf90f9ba5ec072295018
Emmanuel <emmanuel@ryom.eu>
2020-04-15 17:20:30 +0200

    Clients is a map now

--- src/main/dataStructure.java
+++ src/main/dataStructure.java
@@ -1,93 +1,66 @@
 package main;
 
 
+import java.util.Map;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 // Emmanuel Ryom (s184371)
-class client extends dataStructure{
-	String Name;
-	int cID;
-	String Address;
-	String Email;
-	int Phone;
-}
-
 class IDgen extends dataStructure{
-	public static int cIDgen(ArrayList<client> cs) {
-		boolean copy = true;
+	public static int cIDgen() {
 		int newID = 0;
-		while(copy==true) {
-			copy = false;
+		while(true) {
 			newID = (int) (0 + Math.floor(Math.random() * (( clientsSize - 0 ) +1 )));
-			for (int i = 0; i < cs.size(); i++) {
-				if (cs.get(i).cID==newID) {
-					copy = true;
-				}
-			}			
+			if (clients.get(newID) == null) {
+				return newID;
+			}
 		}
-		return newID;
 	}
 }
 
 public class dataStructure {
-	static ArrayList<client> clients= new ArrayList<client>();
+	static Map<Integer, List<String>> clients= new HashMap<Integer, List<String>>();
+	// Map structure [client id, [name, password, address, email, phone]]
+	//                              0      1         2       3      4
 	static int clientsSize = 1000;
-	static int searchName(String n) {
+	static int search(String keyword, int type) {
 		for (int i =0; i<clientsSize; i++) {
-			if (n.equals(clients.get(i).Name)) {
+			if (clients.get(i) == null) {continue;}
+			if (keyword.equals(clients.get(i).get(type))) {
+				System.out.println("YAY");
 				return i;
 			}
 		}
 		return -1;
 	}
-	static int searchAddress(String n) {
-		for (int i =0; i<clientsSize; i++) {
-			if (n.equals(clients.get(i).Address)) {
-				return i;
-			}
-		}
-		return -1;
+	public static void regNewClient(String name, String password, String address, String email, String phone) {
+		ArrayList<String> vars = new ArrayList<String>();
+		vars.add(name);
+		vars.add(password);
+		vars.add(address);
+		vars.add(email);
+		vars.add(phone);
+		clients.put(IDgen.cIDgen(), vars);		
 	}
-	static int searchEmail(String n) {
-		for (int i =0; i<clientsSize; i++) {
-			if (n.equals(clients.get(i).Email)) {
-				return i;
-			}
-		}
-		return -1;
+	public static void updateClient(int ID, String name, String password, String address, String email, String phone) {
+		ArrayList<String> vars = new ArrayList<String>();
+		vars.add(name);
+		vars.add(password);
+		vars.add(address);
+		vars.add(email);
+		vars.add(phone);
+		clients.put(ID,vars);	
 	}
-	static int searchPhone(int n) {
-		for (int i =0; i<clientsSize; i++) {
-			if (clients.get(i).Phone==n) {
-				return i;
-			}
-		}
-		return -1;
-	}
-	public static void regNewClient(String name, String address, String email, int phone) {
-		client c = new client();
-		c.Name = name;
-		c.Address = address;
-		c.Email = email;
-		c.Phone = phone;
-		c.cID = IDgen.cIDgen(clients);
-		clients.set(c.cID,c);	
-	}
-	public static void updateClient(String name, String address, String email, int phone, int ID) {
-		client c = new client();
-		c.Name = name;
-		c.Address = address;
-		c.Email = email;
-		c.Phone = phone;
-		c.cID = ID;
-		clients.set(c.cID,c);	
-	}
-	
 	public static void main(String[] args) {
-		
-		
-		for (int i =0; i<clientsSize; i++) {
-			clients.add(new client());
-		}		
+//		System.out.println(clients.size());
+//		ArrayList<String> test =  new ArrayList<String>() ;
+//		test.add("John&co");
+//		test.add("Snow");
+//		test.add("275 Brookmere rd");
+//		test.add("John89@johnco.com");
+//		test.add("123456");
+//		clients.put(IDgen.cIDgen(), test);
+//		System.out.println(clients.get(search("John&co",0)));
 	}
 }
