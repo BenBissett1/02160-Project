c2e0f46d5993173a8b50efba4cf4e2c3490fbe8c
SoerenToennesen <sorentoennesen@gmail.com>
2020-04-16 15:06:31 +0200

    Fixed class recognition from StepDefinition

--- src/test/StepDefinition.java
+++ src/test/StepDefinition.java
@@ -5,6 +5,11 @@ import static org.junit.Assert.assertEquals;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import main.Client;
+import main.Container;
+import main.Database;
+import main.Port;
+import main.ResponseObject;
 
 public class StepDefinition {
 	
