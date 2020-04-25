aa4d1ad555cdc82fd7b0a5d234cedfddecace51a
BobTheMurderer <BobTheMurderer@DESKTOP-35AHFQG>
2020-04-25 03:00:43 +0200

    fixed some coverage

--- src/test/StepDefinition.java
+++ src/test/StepDefinition.java
@@ -223,18 +223,13 @@ public class StepDefinition {
 
 
 /*
-
 import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
-
 public class StepDefinition {
-
 	Container container = new Container();
 	tempDataMap tempDataMap = new tempDataMap();
 	responseObject response;
@@ -244,25 +239,21 @@ public class StepDefinition {
 	    // Write code here that turns the phrase above into concrete actions
 	    container.setContainerID(containerID);
 	}
-
 	@Given("journey is {string}")
 	public void journey_is(String journey) {
 	    // Write code here that turns the phrase above into concrete actions
 	    container.setJourney(journey);
 	}
-
 	@Given("specificID is first time occurrence")
 	public void specificid_is_first_time_occurrence() {
 	    // Write code here that turns the phrase above into concrete actions
 	    tempDataMap.doesKeyExist(container.specificID);
 	}
-
 	@Then("Input internal data and store in container")
 	public void input_internal_data_and_store_in_container() {
 	    // Write code here that turns the phrase above into concrete actions
 	    container.setData();
 	}
-
 	@Then("transfer data from tempDataMap to dataMap")
 	public void transfer_data_from_tempDataMap_to_dataMap() {
 	    // Write code here that turns the phrase above into concrete actions
@@ -272,7 +263,6 @@ public class StepDefinition {
 	    
 	    response = new responseObject(200, "data stored");
 	}
-
 	@Then("Display that data has been registered")
 	public void display_that_data_has_been_registered() {
 	    // Write code here that turns the phrase above into concrete actions
@@ -288,7 +278,6 @@ public class StepDefinition {
 	    container.outHum = tempDataMap.dataMap.get(container.holdID2);
 	    container.outTemp = tempDataMap.dataMap.get(container.holdID3);
 	}
-
 	@When("retrieve data {string}")
 	public void retrieve_data(String dataType) {
 	    // Write code here that turns the phrase above into concrete actions
@@ -303,13 +292,11 @@ public class StepDefinition {
 //		response.getErrorMessage().equals("single dataType stored");
 		throw new io.cucumber.java.PendingException();
 	}
-
 	@When("display no data available for specificID")
 	public void display_no_data_available_for_specificID() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
-
 	@When("display no data available invalid specificID")
 	public void display_no_data_available_invalid_specificID() {
 	    // Write code here that turns the phrase above into concrete actions
