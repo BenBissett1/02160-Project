1c9aed1589a3384ea729cd5b2af726f4d5355e2a
Emmanuel <emmanuel@ryom.eu>
2020-04-25 12:52:29 +0200

    back to workable conditions

--- src/main/dataStructure.java
+++ src/main/dataStructure.java
@@ -10,11 +10,6 @@ import java.util.List;
 import java.util.Map;
 
 public class dataStructure {
-	
-	boolean isCompleteDataSet;
-	Container container2 = new Container();
-	
-	
 	public static int generate(int max, int type) {
 		int newID = 0;
 		while(true) {
@@ -32,7 +27,7 @@ public class dataStructure {
 		}
 	}
 	
-	/*public static ResponseObject register(Client client, Port port, Container container) {
+	public static ResponseObject register(Client client, Port port, Container container) {
 		ResponseObject response;
 		if (!container.isRegistered()) {
 			
@@ -42,15 +37,15 @@ public class dataStructure {
 				response = new ResponseObject(220, "Registration unsuccessful");
 			} else {
 				container.setRegistered(true);
-				response = new ResponseObject(200, "Registration successful"); 
+				response = new ResponseObject(200, "Registration successful");
 			}
 		} else {
 			response = new ResponseObject(210, "Already registered");
 		}
 		return response;
-	}*/
+	}
 	
-	public static ResponseObject position(Container container) {
+	public ResponseObject position(Container container) {
 		ResponseObject response;
 		
 		if (container.getJourneyID() < 1000) {
@@ -67,121 +62,83 @@ public class dataStructure {
 	
 	
 	
+	public static Map<Integer, List<String>> clients= new HashMap<Integer, List<String>>();
+	// Map structure [client id, [name, password, address, email, phone]]
+	//                              0      1         2       3      4
+	public static Map<Integer, List<List<String>>> journeys= new HashMap<Integer, List<List<String>>>();
+	// Map structure [Journey ID, [[origin, destination, status,  content, ClientID], [val1 list], [val2 list], [val3 list]]]
+	//                                00         01         02       03       04          10           20           30
 	static int clientsSize = 1000;
 	static int journeysSize = 100000;
-	public static Map<Integer, Client> clients= new HashMap<Integer, Client>();
-	public static Map<Integer, Journey> journeys= new HashMap<Integer, Journey>();
 	public static int searchC(String keyword, int type) {
 		for (int i =clientsSize; i<2*clientsSize; i++) {
-			if (type == 0) {
-				if (clients.get(i) == null || clients.get(i).name.equals("")) {continue;}
-				if (keyword.equals(clients.get(i).name)) {
-					return i;}
-			}
-			if (type == 2) {
-				if (clients.get(i) == null || clients.get(i).address.equals("")) {continue;}
-				if (keyword.equals(clients.get(i).address)) {
-					return i;}
-			}
-			if (type == 3) {
-				if (clients.get(i) == null || clients.get(i).email.equals("")) {continue;}
-				if (keyword.equals(clients.get(i).email)) {
-					return i;}
-			}
-			if (type == 4) {
-				if (clients.get(i) == null || clients.get(i).phone.equals("")) {continue;}
-				if (keyword.equals(clients.get(i).phone)) {
-					return i;}
+			if (clients.get(i) == null || clients.get(i).get(0).equals("")) {continue;}
+			if (keyword.equals(clients.get(i).get(type))) {
+				return i;
 			}
 		}
 		return -1;
 	} 
 	public static List<Integer> searchJ(String keyword, int type, int cID) {
 		List<Integer> journeyIDs = new ArrayList<Integer>();
-		System.out.println(journeys);
+//		System.out.println(journeys);
 		for (int i = Integer.parseInt(cID+""+journeysSize); i<Integer.parseInt(cID+""+2*journeysSize); i++) {
-			if (type == 0) {
-				if (journeys.get(i) == null || journeys.get(i).origin.equals("")) {continue;}
-				if (keyword.equals(journeys.get(i).origin)) {
-					journeyIDs.add(i);
-				}
-			}
-			if (type == 1) {
-				if (journeys.get(i) == null || journeys.get(i).destination.equals("")) {continue;}
-				if (keyword.equals(journeys.get(i).destination)) {
-					journeyIDs.add(i);
-				}
-			}
-			if (type == 2) {
-				if (journeys.get(i) == null || journeys.get(i).status.equals("")) {continue;}
-				if (keyword.equals(journeys.get(i).status)) {
-					journeyIDs.add(i);
-				}
-			}
-			if (type == 3) {
-				if (journeys.get(i) == null || journeys.get(i).content.equals("")) {continue;}
-				if (keyword.equals(journeys.get(i).content)) {
-					journeyIDs.add(i);
-				}
-			}
-			if (type == 4) {
-				if (journeys.get(i) == null || journeys.get(i).ClientID.equals("")) {continue;}
-				if (keyword.equals(journeys.get(i).ClientID)) {
-					journeyIDs.add(i);
-				}
+			if (journeys.get(i) == null || journeys.get(i).get(0).get(0).equals("")) {continue;}
+			if (keyword.equals(journeys.get(i).get(0).get(type))) {
+				journeyIDs.add(i);
 			}
 		}
 		return journeyIDs;
 	}
 	public static int regNewClient(String name, String password, String address, String email, String phone) {
-		Client c = new Client();
-		c.name=name;
-		c.password=password;
-		c.address=address;
-		c.email=email;
-		c.phone=phone;
+		ArrayList<String> vars = new ArrayList<String>();
+		vars.add(name);
+		vars.add(password);
+		vars.add(address);
+		vars.add(email);
+		vars.add(phone);
 		if (searchC(name, 0) !=-1 | searchC(address, 2) !=-1 | searchC(email, 3) != -1 | searchC(phone, 4) != -1) {
 			return -1;
 		}
 		else {
 			int cID = generate(clientsSize, 1);
-			clients.put(cID, c);
+			clients.put(cID, vars);
 			return cID;
 		}
 	}
 	public static int regNewJourney(String origin, String destination, String content, int ClientID) {
-		Journey j = new Journey();
-		j.origin=origin;
-		j.destination=destination;
-		j.status="Preparing for departure";
-		j.content=content;
-		j.ClientID=""+ClientID;
-		if (clients.get(ClientID)==null) {
-			return -1;
-		}
-		else {
-			int jID=Integer.parseInt(ClientID+""+generate(journeysSize, 2));
-			journeys.put(jID, j);
-			return jID;
-		}
+		ArrayList<String> vars = new ArrayList<String>();
+		vars.add(origin);
+		vars.add(destination);
+		vars.add("Preparing for departure");
+		vars.add(content);
+		vars.add(""+ClientID);
+		List<List<String>> vars2 = new ArrayList<List<String>>();
+		vars2.add(vars);
+		int jID=Integer.parseInt(ClientID+""+generate(journeysSize, 2));
+		journeys.put(jID, vars2);
+		return jID;
+		
 	}
 	public static void updateClient(int ID, String name, String password, String address, String email, String phone) {
-		Client c = new Client();
-		c.name=name;
-		c.password=password;
-		c.address=address;
-		c.email=email;
-		c.phone=phone;
-		clients.put(ID,c);	
+		ArrayList<String> vars = new ArrayList<String>();
+		vars.add(name);
+		vars.add(password);
+		vars.add(address);
+		vars.add(email);
+		vars.add(phone);
+		clients.put(ID,vars);	
 	}
 	public static void updateJourney(int ID, String origin, String destination, String status, String content, String cID) {
-		Journey j = new Journey();
-		j.origin=origin;
-		j.destination=destination;
-		j.status="Preparing for departure";
-		j.content=content;
-		j.ClientID=""+cID;
-		journeys.put(ID, j);		
+		ArrayList<String> vars = new ArrayList<String>();
+		vars.add(origin);
+		vars.add(destination);
+		vars.add(status);
+		vars.add(content);
+		vars.add(""+cID);
+		List<List<String>> vars2 = new ArrayList<List<String>>();
+		vars2.add(vars);
+		journeys.put(ID, vars2);		
 	}
 	public static void save() {
 		try {
@@ -208,7 +165,7 @@ public class dataStructure {
 	        File toRead=new File("clients");
 	        FileInputStream fis=new FileInputStream(toRead);
 	        ObjectInputStream ois=new ObjectInputStream(fis);
-	        clients=(HashMap<Integer, Client>)ois.readObject();
+	        clients=(HashMap<Integer, List<String>>)ois.readObject();
 	        ois.close();
 	        fis.close();
 	    } catch(Exception e) {}
@@ -216,7 +173,7 @@ public class dataStructure {
 	        File toRead=new File("journeys");
 	        FileInputStream fis=new FileInputStream(toRead);
 	        ObjectInputStream ois=new ObjectInputStream(fis);
-	        journeys=(HashMap<Integer, Journey>)ois.readObject();
+	        journeys=(HashMap<Integer, List<List<String>>>)ois.readObject();
 	        ois.close();
 	        fis.close();
 	    } catch(Exception e) {}
@@ -237,4 +194,5 @@ public class dataStructure {
 			return true;
 		}
 	}
+	
 }
