bda3996de08c5e36fb1a64528c7f598b04fe0e8d
Benjamin Bissett <ben.bissett@outlook.com>
2020-04-26 18:51:52 +0200

    interface update

--- src/UI/companyUpdateJourney.java
+++ src/UI/companyUpdateJourney.java
@@ -24,20 +24,10 @@ import main.Container;
 import main.dataStructure;
 
 import javax.swing.border.BevelBorder;
-
-
-
-
-///////TODO Status dropdown menu
-
-
-
-
+import javax.swing.JComboBox;
 
 public class companyUpdateJourney extends JFrame {
 	private JTextField journeyIDField;
-	private JTextField destinationField;
-	private JTextField statusField;
 	public companyUpdateJourney() {
 		super("Update a Journey");
 		setSize(new Dimension(313, 300));
@@ -54,6 +44,11 @@ public class companyUpdateJourney extends JFrame {
 		panel.setBackground(Color.LIGHT_GRAY);
 		getContentPane().add(panel);
 		panel.setLayout(null);
+		
+		String[] mapLocations = { "Copenhagen", "New York", "Toronto", "Edinburgh", "Busan", "Shenzhen", "Shanghai", "Tanjung", "Dubai", "Los Angeles", "Valencia",
+                "Santos", "Tanger Med", "Salalah", "Cape Town", "Colombo", "Comodoro", "Christchurch", "Perth", "Mogadishu"};
+		
+		String[] statuses = { "At Origin", "At Sea", "Arrived"};
 		
 		JLabel header = new JLabel("Please fill in the form below");
 		header.setBounds(0, 15, 310, 20);
@@ -84,13 +79,6 @@ public class companyUpdateJourney extends JFrame {
 		companyUpdateDestination.setBounds(55, 115, 100, 20);
 		panel.add(companyUpdateDestination);
 		
-		destinationField = new JTextField();
-		destinationField.setHorizontalAlignment(SwingConstants.CENTER);
-		destinationField.setFont(new Font("Tahoma", Font.BOLD, 13));
-		destinationField.setColumns(10);
-		destinationField.setBounds(160, 115, 95, 25);
-		panel.add(destinationField);
-		
 		JLabel companyUpdateStatus = new JLabel("Status:");
 		companyUpdateStatus.setHorizontalTextPosition(SwingConstants.CENTER);
 		companyUpdateStatus.setHorizontalAlignment(SwingConstants.CENTER);
@@ -98,12 +86,15 @@ public class companyUpdateJourney extends JFrame {
 		companyUpdateStatus.setBounds(55, 155, 95, 20);
 		panel.add(companyUpdateStatus);
 		
-		statusField = new JTextField();
-		statusField.setHorizontalAlignment(SwingConstants.CENTER);
-		statusField.setFont(new Font("Tahoma", Font.BOLD, 13));
-		statusField.setColumns(10);
-		statusField.setBounds(160, 155, 95, 25);
-		panel.add(statusField);
+		JComboBox updateJourneyDestinationComboBox = new JComboBox(mapLocations);
+		updateJourneyDestinationComboBox.setFont(new Font("Tahoma", Font.BOLD, 13));
+		updateJourneyDestinationComboBox.setBounds(160, 115, 95, 25);
+		panel.add(updateJourneyDestinationComboBox);
+		
+		JComboBox updateJourneyStatusComboBox = new JComboBox(statuses);
+		updateJourneyStatusComboBox.setFont(new Font("Tahoma", Font.BOLD, 13));
+		updateJourneyStatusComboBox.setBounds(160, 155, 95, 25);
+		panel.add(updateJourneyStatusComboBox);
 		
 		JButton backButton = new JButton("Back");
 		backButton.setFont(new Font("Tahoma", Font.BOLD, 16));
@@ -126,8 +117,8 @@ public class companyUpdateJourney extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String inputJourneyID = journeyIDField.getText();
-				String inputDestination = destinationField.getText();
-				String inputStatus = statusField.getText();
+				String inputDestination = (String) updateJourneyDestinationComboBox.getSelectedItem();
+				String inputStatus = (String) updateJourneyStatusComboBox.getSelectedItem();
 				if (inputJourneyID.isEmpty() || inputDestination.isEmpty() || inputStatus.isEmpty()) {
 					JOptionPane.showMessageDialog(panel,
 							"All fields must be completed",
@@ -162,6 +153,7 @@ public class companyUpdateJourney extends JFrame {
 		});
 		panel.add(confirmButton);
 	}
+	
 	public static void companyUpdateJourney() {
 		companyUpdateJourney cmpUdtJrn = new companyUpdateJourney();
 		cmpUdtJrn.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
