320bbc4f65de330d3bc346d6fcb9bb55723791e0
demNuts <s184444@win.dtu.dk>
2020-05-06 01:13:53 +0200

    added javadoc comments

--- src/main/Journey.java
+++ src/main/Journey.java
@@ -1,3 +1,7 @@
+/**
+* @since   2020-05-06 
+*/
+
 package main;
 
 import java.util.ArrayList;
@@ -139,6 +143,14 @@ public class Journey extends dataStructure {
 	}
 	
 	//Journey journey;
+	/**
+	 * Updates one value for each internal status parameter for a certain container.
+	 * @param journeyID integer specific id for this journey.
+	 * @param temp float temperature [celsius]
+	 * @param hum float humidity [g/m3]
+	 * @param pres float pressure [atm]
+	 */
+	
 	public void updateContainerStatus(int journeyID,float temp, float hum, float pres) {
 		if (dataStructure.journeys.get(journeyID) != null) {
 			Journey j = dataStructure.journeys.get(journeyID);
@@ -151,7 +163,7 @@ public class Journey extends dataStructure {
 		}
 		
 	}
-	
+
 	public boolean doesJourneyExist() {
 		if (dataStructure.journeys.containsKey(journeyID)) {
 //			journey = dataStructure.journeys.get(journeyID);
